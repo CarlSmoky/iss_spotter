8da056958f07224892317efdd7dcf579d7693e01
tsumitakaoru <Katherine@KT-MacBook-Air-2.local>
2021-11-04 20:25:31 -0400

    add fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 
 // The code below is temporary and can be commented out.
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -15,12 +15,22 @@ fetchMyIP((error, ip) => {
 });
 
 // The code below is temporary and can be commented out.
-
-fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+const ip = '162.245.144.188';
+fetchCoordsByIP(ip, (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
   console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+const coords = { latitude: '49.27670', longitude: '-123.13000' };
+fetchISSFlyOverTimes(coords, (error, flyover) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned fly over:' , flyover);
 });
